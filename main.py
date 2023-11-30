ea5032d64762e6bea6cbeef29ed367d01ce4a8ab
Raouf <raouf@yzr.ai>
2023-11-30 10:29:23 +0100

    Add UMAP method to main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from sklearn.datasets import fetch_20newsgroups
 from sklearn.metrics.cluster import normalized_mutual_info_score, adjusted_rand_score
 from sentence_transformers import SentenceTransformer
 import numpy as np
-
+from sklearn.cluster import KMeans
+import umap
 
 def dim_red(mat, p, method):
     '''
@@ -24,6 +25,8 @@ def dim_red(mat, p, method):
         
     elif method=='UMAP':
         red_mat = mat[:,:p]
+        reducer = umap.UMAP(n_components=p)
+        red_mat = reducer.fit_transform(mat)
         
     else:
         raise Exception("Please select one of the three methods : APC, AFC, UMAP")
@@ -43,9 +46,9 @@ def clust(mat, k):
     ------
         pred : list of predicted labels
     '''
-    
-    pred = np.random.randint(k, size=len(corpus))
-    
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    pred = kmeans.fit_predict(mat)
+
     return pred
 
 # import data
